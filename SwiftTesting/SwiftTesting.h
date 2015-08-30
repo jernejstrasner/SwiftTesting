c94f5a7027899148674aac4b83382d80c9c8ff70
Jernej Strasner <jernej.strasner@gmail.com>
2015-08-30 16:32:38 +0200

    Initial Commit

---
+++ SwiftTesting/SwiftTesting.h
@@ -0,0 +1,19 @@
+//
+//  SwiftTesting.h
+//  SwiftTesting
+//
+//  Created by Jernej Strasner on 8/30/15.
+//  Copyright © 2015 Jernej Strasner. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwiftTesting.
+FOUNDATION_EXPORT double SwiftTestingVersionNumber;
+
+//! Project version string for SwiftTesting.
+FOUNDATION_EXPORT const unsigned char SwiftTestingVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftTesting/PublicHeader.h>
+
+
